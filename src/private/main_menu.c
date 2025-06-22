fa81ed70d64cd8cf4ea80b268e9546b28a78495a
MarianofK <kfmarian@protonmail.com>
2025-06-22 19:05:23 -0300

    Change some variable names to avoid collisions

--- src/private/main_menu.c
+++ src/private/main_menu.c
@@ -1,40 +1,42 @@
 #include "main_menu.h"
 
+Rectangle main_menu_btnPlay = {0, 0, rectangleWidth, rectangleHeight};
+Rectangle main_menu_btnOptions = {0, 0, rectangleWidth, rectangleHeight};
+Rectangle main_menu_btnExit = {0, 0, rectangleWidth, rectangleHeight};
 
-Rectangle btnPlay    = { 0, 0, rectangleWidth, rectangleHeight };
-Rectangle btnOptions = { 0, 0, rectangleWidth, rectangleHeight };
-Rectangle btnExit    = { 0, 0, rectangleWidth, rectangleHeight };
+void DrawMainMenu(int x, int y) {
 
-void DrawMenu(int x, int y) {
+  DrawText("The Archer", x - (rectangleWidth / 2 - 7.5), y - 130, 30, BLACK);
 
-    DrawText("The Archer", x - (rectangleWidth/2 - 7.5), y - 130, 30, BLACK);
+  main_menu_btnPlay.x = x - rectangleWidth / 2;
+  main_menu_btnPlay.y = y - 60;
+  DrawRectangleRec(main_menu_btnPlay, GetButtonColor(main_menu_btnPlay));
+  DrawText("Empezar", main_menu_btnPlay.x + (rectangleWidth / 2 - 40),
+           main_menu_btnPlay.y + 15, 20, BLACK);
 
-    btnPlay.x = x - rectangleWidth/2;
-    btnPlay.y = y - 60;
-    DrawRectangleRec(btnPlay, GetButtonColor(btnPlay));
-    DrawText("Empezar", btnPlay.x + (rectangleWidth/2 - 40), btnPlay.y + 15, 20, BLACK);
+  main_menu_btnOptions.x = x - rectangleWidth / 2;
+  main_menu_btnOptions.y = y;
+  DrawRectangleRec(main_menu_btnOptions, GetButtonColor(main_menu_btnOptions));
+  DrawText("Ajustes", main_menu_btnOptions.x + (rectangleWidth / 2 - 40),
+           main_menu_btnOptions.y + 15, 20, BLACK);
 
-    btnOptions.x = x - rectangleWidth/2;
-    btnOptions.y = y;
-    DrawRectangleRec(btnOptions, GetButtonColor(btnOptions));
-    DrawText("Ajustes", btnOptions.x + (rectangleWidth/2 - 40), btnOptions.y + 15, 20, BLACK);
-
-    btnExit.x = x - rectangleWidth/2;
-    btnExit.y = y + 60;
-    DrawRectangleRec(btnExit, GetButtonColor(btnExit));
-    DrawText("Salir", btnExit.x + (rectangleWidth/2 - 25), btnExit.y + 15, 20, BLACK);
+  main_menu_btnExit.x = x - rectangleWidth / 2;
+  main_menu_btnExit.y = y + 60;
+  DrawRectangleRec(main_menu_btnExit, GetButtonColor(main_menu_btnExit));
+  DrawText("Salir", main_menu_btnExit.x + (rectangleWidth / 2 - 25),
+           main_menu_btnExit.y + 15, 20, BLACK);
 }
 
-GameState UpdateMenu() {
-    Vector2 mouse = GetMousePosition();
-    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-        if (CheckCollisionPointRec(mouse, btnPlay)) {
-            return STATE_GAME;
-        } else if (CheckCollisionPointRec(mouse, btnOptions)) {
-            return STATE_SETTINGS;
-        } else if (CheckCollisionPointRec(mouse, btnExit)) {
-            return STATE_EXIT;
-        }
+GameState UpdateMainMenu() {
+  Vector2 mouse = GetMousePosition();
+  if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+    if (CheckCollisionPointRec(mouse, main_menu_btnPlay)) {
+      return STATE_GAME;
+    } else if (CheckCollisionPointRec(mouse, main_menu_btnOptions)) {
+      return STATE_SETTINGS;
+    } else if (CheckCollisionPointRec(mouse, main_menu_btnExit)) {
+      return STATE_EXIT;
     }
-    return STATE_MENU;
+  }
+  return STATE_MENU;
 }
--- src/public/main_menu.h
+++ src/public/main_menu.h
@@ -1,17 +1,18 @@
 #ifndef MAIN_MENU_H
 #define MAIN_MENU_H
 
-#include "raylib.h"
 #include "definitions.h"
 #include "functions.h"
+#include "raylib.h"
 
-/// @brief Dibuja el menu centrado con respecto a un punto, mostrando un titulo y las opciones de empezar, ajustes y salir.
+/// @brief Dibuja el menu centrado con respecto a un punto, mostrando un titulo
+/// y las opciones de empezar, ajustes y salir.
 /// @param x Posicion del menu en X
 /// @param y Posicion del menu en Y
-void DrawMenu(int x, int y);
+void DrawMainMenu(int x, int y);
 
 /// @brief Detecta la colision del mouse y actualiza el estado del juego.
 /// @return Devuelve un valor del enum GameState.
-GameState UpdateMenu();
+GameState UpdateMainMenu();
 
 #endif
